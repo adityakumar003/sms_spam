f821bde3f27bddcbcf82f4385c49fa010f4f685c
adityakumar003 <adiiv8609@gmail.com>
2025-03-10 23:19:25 +0530

    Update smsapp.py

--- smsapp.py
+++ smsapp.py
@@ -1,12 +1,15 @@
 import streamlit as st
 import pickle
 import string
-from nltk.corpus import stopwords
 import nltk
+from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 
-ps = PorterStemmer()
+# Download required NLTK data
+nltk.download('punkt')
+nltk.download('stopwords')
 
+ps = PorterStemmer()
 
 def transform_text(text):
     text = text.lower()
@@ -32,22 +35,23 @@ def transform_text(text):
 
     return " ".join(y)
 
-tfidf = pickle.load(open('vectorizer (1).pkl','rb'))
-model = pickle.load(open('model (1).pkl','rb'))
+# Load models
+tfidf = pickle.load(open('vectorizer (1).pkl', 'rb'))
+model = pickle.load(open('model (1).pkl', 'rb'))
 
+# Streamlit UI
 st.title("Email/SMS Spam Classifier")
 
 input_sms = st.text_area("Enter the message")
 
 if st.button('Predict'):
-
-    # 1. preprocess
+    # 1. Preprocess
     transformed_sms = transform_text(input_sms)
-    # 2. vectorize
+    # 2. Vectorize
     vector_input = tfidf.transform([transformed_sms])
-    # 3. predict
+    # 3. Predict
     result = model.predict(vector_input)[0]
-    # 4. Display
+    # 4. Display result
     if result == 1:
         st.header("Spam")
     else:
